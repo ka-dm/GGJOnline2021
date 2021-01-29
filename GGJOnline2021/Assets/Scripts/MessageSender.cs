6635571d116887603e9581a4f5a5fad8f77c1374
Quimi <david.crespo@correounivalle.edu.co>
2021-01-29 13:15:53 -0500

    Listo sistema de mensajería
    Cuenta el número de carácteres y mensajes disponibles
    Cuando se está escribiendo texto el input del jugador se desactiva

--- GGJOnline2021/Assets/Scripts/MessageSender.cs
+++ GGJOnline2021/Assets/Scripts/MessageSender.cs
@@ -9,20 +9,35 @@ public delegate void SendMessage(string message);
 public class MessageSender : MonoBehaviour
 {
     [SerializeField] TMP_InputField messageField;
+    [SerializeField] TextMeshProUGUI characterCounter;
     [SerializeField] int messagesAvailable = 3;
     public static event SendMessage sendMessage;
+
+    [SerializeField] GameObject availableMessagesCounter;
+    [SerializeField] GameObject mailIconPrefab;
+
+    private void Start()
+    {
+        messageField.onValueChanged.AddListener(delegate { UpdateCharacterCounter(); });
+        messageField.onSelect.AddListener(delegate { SwitchToMessageInputMode(); });
+        messageField.onDeselect.AddListener(delegate { SwitchToGameplayMode(); });
+
+        UpdateCharacterCounter();
+        UpdateAvailableMessagesCounter();
+    }
     public void OnSendMessage(InputValue input)
     {
         if(messageField.text != "")
         {
             HandleMessage();
+            SwitchToGameplayMode();
+            UpdateAvailableMessagesCounter();
         }
 
     }
 
     private void HandleMessage()
     {
-        Debug.Log(messageField.text);
         sendMessage(messageField.text);
         messageField.text = "";
         messagesAvailable--;
@@ -33,5 +48,38 @@ public class MessageSender : MonoBehaviour
     {
         if (messagesAvailable == 0)
             messageField.interactable = false;
+    }
+
+    private void UpdateCharacterCounter()
+    {
+        characterCounter.text = string.Format("{0} / {1}", messageField.text.Length, messageField.characterLimit);
+    }
+
+    private void UpdateAvailableMessagesCounter()
+    {
+        RemoveEveryAvailableMessageCounter();
+        for (int i = 0; i < messagesAvailable; i++)
+        {
+            var go = Instantiate(mailIconPrefab);
+            go.transform.parent = availableMessagesCounter.transform;
+        }   
+    }
+
+    private void RemoveEveryAvailableMessageCounter()
+    {
+        foreach (Transform child in availableMessagesCounter.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    private void SwitchToMessageInputMode()
+    {
+        this.gameObject.GetComponent<Movement>().enabled = false;
+    }
+
+    private void SwitchToGameplayMode()
+    {
+        this.gameObject.GetComponent<Movement>().enabled = true;
     }
 }
