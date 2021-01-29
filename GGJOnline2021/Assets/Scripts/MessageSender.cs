bd0bbf8343a00d710c7419e5240ea00b8c20a1bf
Quimi <david.crespo@correounivalle.edu.co>
2021-01-28 23:49:05 -0500

    Casi listo sistema de mensajería

--- GGJOnline2021/Assets/Scripts/MessageSender.cs
+++ GGJOnline2021/Assets/Scripts/MessageSender.cs
@@ -4,12 +4,34 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public delegate void SendMessage(string message);
+
 public class MessageSender : MonoBehaviour
 {
     [SerializeField] TMP_InputField messageField;
+    [SerializeField] int messagesAvailable = 3;
+    public static event SendMessage sendMessage;
     public void OnSendMessage(InputValue input)
     {
+        if(messageField.text != "")
+        {
+            HandleMessage();
+        }
+
+    }
+
+    private void HandleMessage()
+    {
         Debug.Log(messageField.text);
+        sendMessage(messageField.text);
         messageField.text = "";
+        messagesAvailable--;
+        VerifyRemianingMessages();
+    }
+
+    private void VerifyRemianingMessages()
+    {
+        if (messagesAvailable == 0)
+            messageField.interactable = false;
     }
 }
---
+++ GGJOnline2021/Assets/Scripts/PastMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PastMessage : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI message;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MessageSender.sendMessage += (string s) => message.text = s;
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
