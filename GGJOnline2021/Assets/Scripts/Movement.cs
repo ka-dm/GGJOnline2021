c0d2485ca283ad49b4f993d53070e4467fa6b5a4
Quimi <david.crespo@correounivalle.edu.co>
2021-01-29 21:54:06 -0500

    No sé qué estoy haciendo
    XDDDDDDDDDDDDDDDDDDD

--- GGJOnline2021/Assets/Scripts/Movement.cs
+++ GGJOnline2021/Assets/Scripts/Movement.cs
@@ -9,6 +9,7 @@ public class Movement : MonoBehaviour
     [SerializeField] PlayerInput playerInput;
     [SerializeField] float moveSpeed = 3F;
     Vector3 moveVector;
+    Vector2 xd;
 
     void FixedUpdate()
     {
@@ -22,8 +23,10 @@ public class Movement : MonoBehaviour
 
     public void OnMove(InputValue input)
     {
+        xd = input.Get<Vector2>();
         Vector2 vec = input.Get<Vector2>();
         moveVector = new Vector3(vec.x, 0, vec.y) * moveSpeed * Time.deltaTime;
+        Debug.Log(xd);
     }
 
 
