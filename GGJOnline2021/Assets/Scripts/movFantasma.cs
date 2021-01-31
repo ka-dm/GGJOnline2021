bab8832eca2b45b561cfc526cf3ea243da3d3df8
seibern <melissa.fuentes@correounivalle.edu.co>
2021-01-31 03:25:30 +0100

    fantasmas
    basico para escenario de fantasmitas

---
+++ GGJOnline2021/Assets/Scripts/movFantasma.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movFantasma : MonoBehaviour
+{
+    public Transform objectFollow;
+    public float velocidadMovimiento;
+    Vector3 movimiento = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int enemigoSeguir = Random.Range(0, 2);
+
+        if (enemigoSeguir == 1)
+        {
+            objectFollow = GameObject.FindGameObjectWithTag("Player1").GetComponent<Transform>();
+        }
+        else{
+            objectFollow = GameObject.FindGameObjectWithTag("Player2").GetComponent<Transform>();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(objectFollow.position);
+        transform.Translate(0.0f, 0.0f, velocidadMovimiento*Time.deltaTime);
+
+        }
+
+    public void OnTriggerEnter(Collider collision) 
+    {
+       // if (collision.tag == "base")
+            Destroy(gameObject);
+    }
+}
+
